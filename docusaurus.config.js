4f5ac365c7f03a149c9132bde609b83a570dada7
Lewis Sheridan <lewis@backplane.cloud>
2024-03-06 04:59:00 +0000

    to allow netlify build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // organizationName: "backplane-cloud", // Usually your GitHub org/user name.
   // projectName: "backplane-www", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
