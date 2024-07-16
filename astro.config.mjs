2367e243129bd9e68acb5970cd7df6508de1445c
Lewis Sheridan <lewis@backplane.cloud>
2024-07-16 13:01:36 +0100

    Added partytown and Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://backplane.dev",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
